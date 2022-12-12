6295256ed391df4d71a87e5095b3a6b08a376fe4
Marina Ivanchenko <sunshine808@ya.ru>
2022-12-13 00:10:42 +0300

    add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение M и N. Напишите программу,которая найдет 
+//сумму натуральных чисел в промежутке от N до M.
+//выполнить с помощью рекурсии.
+
+int start = ReadInt("Enter N: ");
+int end = ReadInt("Enter M: ");
+if (end<start)
+{
+    int temp = start;
+    start = end;
+    end = temp;
+}
+Console.Write($"N = {start}; M = {end} -> ");
+Console.Write(SumNaturalNumbers(start,end));
+
+int SumNaturalNumbers(int n, int m)
+{
+    if (m==n)        
+        return m;
+    return m + SumNaturalNumbers(n,m-1);
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
